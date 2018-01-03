8904e14241cdab5f619873723014eba532b3d743
Alasderp <AlasdairStuart@live.co.uk>
2018-01-03 16:20:52 +0000

    Mucking about with react

---
+++ reactapp/src/App.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import song from './National Anthem of USSR.mp3'
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      stateText: 
+      [
+        {
+            id:1,
+            name:"Koshi",
+            age:12,
+            species:"Collie-Poodle"
+        },
+        {
+          id:2,
+          name:"Rosha",
+          age:14,
+          species:"Labrador"          
+        }
+      ]
+    }
+  }
+
+    addDogBtn() {
+      //Get values from text boxes
+      let id = document.getElementById("idInput").value;
+      let name = document.getElementById("nameInput").value;
+      let age = document.getElementById("ageInput").value;
+      let breed = document.getElementById("breedInput").value;
+
+      //Create json object from values
+      let payload = {
+        "id":id,
+        "name":name,
+        "age":age,
+        "species":breed
+      };
+      
+      let tempArray = this.state.stateText.slice();
+
+      tempArray.push(payload);
+
+      this.setState({stateText: tempArray})
+
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+          <br/>{this.props.exampleProp}
+          </p>
+          <br/>
+        <audio controls autoPlay>
+          <source src={song} type="audio/mpeg"/>
+        </audio>
+        <br/>
+        Id:
+        <input id="idInput" type="text"/><br/>
+        Name:
+        <input id="nameInput" type="text"/><br/>
+        Age:
+        <input id="ageInput" type="text"/><br/>
+        Breed:
+        <input id="breedInput" type="text"/><br/>
+        <input type="button" id="addDog" value="Add dog" onClick={this.addDogBtn.bind(this)}/><br/>
+        <input type="button" id="getDog" value="Get Dog"/><br/>
+        <input type="button" id="updateDog" value="Edit Dog"/><br/>
+        <input type="button" id="deleteDog" value="Delete Dog"/><br/>
+        <input type="button" id="allDogs" value="All Dogs"/><br/>
+        
+        <div id="dogContainer">
+          {
+              this.state.stateText.map((dog) => (
+                <p key = {dog.id}>Name: {dog.name} Age: {dog.age} Breed: {dog.species}</p>
+              ))
+          }
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
